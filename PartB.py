d146b4de755243ad47fecbff3a71db7ffefffd7c
B00105225 Perlix Soco <B00105225@mytudublin.ie>
2024-01-31 12:17:38 +0000

    part B and Part c done

---
+++ PartB.py
@@ -0,0 +1,17 @@
+#PerlixSoco
+#31.01.2024
+#Modified PartB.py
+
+def printSteps(a, b):
+    for i in range(a, b+1):
+        print(f"Steps{i}")
+
+print("Step 1")
+print("Step 2")
+print("Step 3")
+
+printSteps(4,6)
+print("Step 7")
+print("Step 8")
+print("Step 9")
+
---
+++ PartC.py
@@ -0,0 +1,9 @@
+#PerlixSoco
+#31.01.2024
+#Modified PartC.py
+
+def printSteps(a, b):
+    for i in range(a, b+1):
+        print(f"Steps{i}")
+
+printSteps(1,9)
